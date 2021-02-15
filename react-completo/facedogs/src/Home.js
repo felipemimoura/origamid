2dcf5af01631ed704be1c076d60847fa0353bf85
felipemimoura <felipemimoura@gmail.com>
2021-02-15 16:44:50 -0300

    resolução dos desafios de componentes

---
+++ react-completo/facedogs/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+export default function Home() {
+    return (
+        <section>
+            <Titulo  texto="home"/>
+            <p>Essa é home</p>
+        </section>
+    )
+}
