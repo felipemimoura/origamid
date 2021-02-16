674e4120b1122127b351572015dfc95a1f818ccd
felipemimoura <felipemimoura@gmail.com>
2021-02-16 17:02:43 -0300

    aulas sobre custom hooks

--- react-completo/aulas/react-hooks/src/Produto.js
+++ react-completo/aulas/react-hooks/src/Produto.js
@@ -1,16 +1,14 @@
-import React, { useContext }  from "react";
-import {GlobalContext} from  './GlobalContext'
+import React from "react";
+
 
 
 export default function Produto() {
-  const global = useContext(GlobalContext)
-  console.log(global.dados)
 
-  if(global.dados === null) return null
+
+ 
   return (
     <div>
-      <div>Produto: {global.dados.map(produto  => <li key={produto.id}>{produto.nome}</li>)}</div>
-      <button onClick={global.limparDados}>Limpar</button>
+
     </div>
   );
 }
