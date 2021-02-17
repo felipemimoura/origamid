fa6dd49b61cdb037e41ab3b512c765fff222c710
felipemimoura <felipemimoura@gmail.com>
2021-02-17 16:41:20 -0300

    criação de elementos de frameworks css junto com react

--- react-completo/aulas/react-css/src/App.js
+++ react-completo/aulas/react-css/src/App.js
@@ -1,11 +1,25 @@
-import Produto from "./Components/Produto";
-
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
-    <div  >
-     <Produto />
-    </div>
+    <>
+      <h1>Com bootstrap</h1>
+      <div className="card bg-dark text-white m-5" style={{maxWidth: "18rem"}}>
+        <div className="card-header">Notebook</div>
+        <div className="card-body">
+          <h5 className="card-title">R$ 2500,00</h5>
+          <p className="card-text">teste teste</p>
+        </div>
+      </div>
+      <h1>Com React-bootstrap</h1>
+      <Card bg="dark" text="white" className="m-5" style={{maxWidth: "18rem"}}>
+        <Card.Header>Notebook</Card.Header>
+        <Card.Body>
+          <Card.Title>R$ 2500,00</Card.Title>
+          <Card.Text>Teste teste</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
