1d25eda3b2f79d3d4bda2915ee2e2abed168487f
felipemimoura <felipemimoura@gmail.com>
2021-02-18 14:54:47 -0300

    inicio do projeto

--- react-completo/facedogs/src/App.js
+++ react-completo/facedogs/src/App.js
@@ -1,27 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Home from "./Home";
-import Produtos from "./Produtos";
-
-
 
 function App() {
-    let Pagina 
-    const {pathname} = window.location
-    if(pathname === '/produtos'){
-        Pagina = Produtos
-    }else{
-        Pagina = Home
-    }
-
-  return (
-      <section>
-          <Header />
-          <Pagina />
-         
-
-      </section>
-  );
+  return <div>App React</div>;
 }
 
 export default App;
--- react-completo/facedogs/src/Titulo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Titulo({texto}) {
-    return (
-        <h1 style={{color: "#4c8b64"}}>
-            {texto}
-        </h1>
-    )
-}
