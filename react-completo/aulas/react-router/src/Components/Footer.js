5f53a6fbfa975a4833d6fe406c4d7f02b2de6dda
felipemimoura <felipemimoura@gmail.com>
2021-02-18 13:29:39 -0300

    Desafio de rotas

---
+++ react-completo/aulas/react-router/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <h1>Alguns direitoos reservados
+                
+            </h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ react-completo/aulas/react-router/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <nav className={styles.header}>
+      <ul>
+        <li>
+          <NavLink activeClassName={styles.active} className={styles.link} to="/" end>
+            {" "}
+            Produtos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName={styles.active} className={styles.link} to="contato">
+            {" "}
+            Contato
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
