72448c5b93305de0a2ea35a868300ae7ce16e241
felipemimoura <felipemimoura@gmail.com>
2021-02-18 18:15:37 -0300

    estrutura inicial

--- react-completo/facedogs/src/App.js
+++ react-completo/facedogs/src/App.js
@@ -1,11 +1,26 @@
-// import React from "react";
-import Api from './api/Api'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+
+import Footer from "./Components/Footer";
+import Login from "./Components/Login/Login";
 
 function App() {
-  return <div className="container">
-      <Api />
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
