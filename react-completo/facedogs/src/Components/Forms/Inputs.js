cc10100f5b035bd37e46bb60969ba5a8b91bda84
felipemimoura <felipemimoura@gmail.com>
2021-02-19 09:53:41 -0300

    criado custom hooks de formulario e validaçao de email

--- react-completo/facedogs/src/Components/Forms/Inputs.js
+++ react-completo/facedogs/src/Components/Forms/Inputs.js
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-function Input({ label, type, name }) {
+function Input({ label, type, name, value, onChange, error , onBlur}) {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
 
-      <input type={type} id={name} name={name} className={styles.input} />
-      <p className={styles.error}> Error</p>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className={styles.input}
+        onChange={onChange}
+        value={value}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}> {error}</p>}
     </div>
   );
 }
