cd9f75e1b1499b90c78b5d43577e8cc337fda547
felipemimoura <felipemimoura@gmail.com>
2021-02-23 16:37:15 -0300

    criaçao da logica de cadastro de usuario

--- react-completo/facedogs/src/Components/Login/LoginCreate.js
+++ react-completo/facedogs/src/Components/Login/LoginCreate.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { USER_POST } from "../../api";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
+import Button from "../Forms/Button";
+import Input from "../Forms/Inputs";
 
 function LoginCreate() {
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+
+  const {userLogin} = useContext(UserContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if(response.ok) userLogin(username.value, password.value)
+    
+
+  }
+
   return (
-    <div>
-        Login Criar
-    </div>
+    <section className="animeLeft">
+      <h1 className="title">Cadastra-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   );
 }
 
