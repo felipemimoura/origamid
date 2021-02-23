8ee58f7cb1f6cf9a06eec5bd9a573b37bef321d5
felipemimoura <felipemimoura@gmail.com>
2021-02-23 16:16:57 -0300

    aula14 termino da estilizaçao do login

--- react-completo/facedogs/src/Components/Login/Login.js
+++ react-completo/facedogs/src/Components/Login/Login.js
@@ -1,24 +1,27 @@
-import React, { useContext } from 'react'
-import {Navigate, Route, Routes} from 'react-router-dom'
-import { UserContext } from '../../UserContext'
-import LoginCreate from './LoginCreate'
-import LoginForm from './LoginForm'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import LoginCreate from "./LoginCreate";
+import LoginForm from "./LoginForm";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import styles from "./Login.module.css";
 
 function Login() {
-    const {login} = useContext(UserContext)
-    if(login === true) return <Navigate to='/conta' />
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={<LoginForm />} />
-                <Route path="criar" element={<LoginCreate />} />
-                <Route path="perdeu" element={<LoginPasswordLost />} />
-                <Route path="resetar" element={<LoginPasswordReset />} />
-            </Routes>
-        </div>
-    )
+  const { login } = useContext(UserContext);
+  if (login === true) return <Navigate to="/conta" />;
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
