c442574b7c4645cde8265634f8c5d3162d9df920
felipemimoura <felipemimoura@gmail.com>
2021-02-23 16:06:09 -0300

    inicio da estilizaçao da pagina de login

--- react-completo/facedogs/src/Components/Login/LoginForm.js
+++ react-completo/facedogs/src/Components/Login/LoginForm.js
@@ -3,50 +3,60 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Inputs";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST, USER_GET} from "../../api";
+import { TOKEN_POST, USER_GET } from "../../api";
 import { UserContext } from "../../UserContext";
+import Error from "../../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 function LoginForm() {
   const username = useForm();
   const password = useForm();
-  const {userLogin, error, loading} = useContext(UserContext)
+  const { userLogin, error, loading } = useContext(UserContext);
 
-
-  useEffect(() =>{
-    const token = window.localStorage.getItem('token')
-    if(token){
-      getUser(token)
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
     }
-  })
+  });
 
-
-  async function getUser(token){
-    const {url, options} = USER_GET(token)
-    const response = await fetch(url, options)
-    const json = await response.json()
- 
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
   }
 
- async function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-     userLogin(username.value, password.value)
-
+      userLogin(username.value, password.value);
     }
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className="animeleft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        {loading ? <Button disable>Carregando ...</Button>:<Button>Entrar</Button> }
-        
-        {error && <p>{error}</p>}
+        {loading ? (
+          <Button disable>Carregando ...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastra-se no site</p>
+        <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+      </div>
     </section>
   );
 }
