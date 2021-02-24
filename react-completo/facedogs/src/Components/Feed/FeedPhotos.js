20edde282836ef87837c460b404dd91706c1d981
felipemimoura <felipemimoura@gmail.com>
2021-02-24 16:09:49 -0300

    criaçao de todo o modal e funcionamento do mesmo

--- react-completo/facedogs/src/Components/Feed/FeedPhotos.js
+++ react-completo/facedogs/src/Components/Feed/FeedPhotos.js
@@ -4,9 +4,9 @@ import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
 import Error from "../../Helper/Error";
 import { Loading } from "../../Helper/Loading";
-import styles from './FeedPhotos.module.css'
+import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
@@ -24,8 +24,13 @@ const FeedPhotos = () => {
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
-          {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} />)}
-        
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
       </ul>
     );
   else {
