e0245d36602395fdc4d00127eddd5ce39d07dd54
felipemimoura <felipemimoura@gmail.com>
2021-02-27 09:29:19 -0300

    logica de deletar foto

--- react-completo/facedogs/src/Components/Photo/PhotoContent.js
+++ react-completo/facedogs/src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
 function PhotoContent({ data }) {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
@@ -13,7 +16,12 @@ function PhotoContent({ data }) {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.visualizacoes}> {photo.acessos}</span>
           </p>
           <h1 className="title">
@@ -25,11 +33,9 @@ function PhotoContent({ data }) {
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments}/>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 }
 
 export default PhotoContent;
-
-
